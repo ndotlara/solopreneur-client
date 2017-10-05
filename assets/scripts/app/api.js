66c5e330626e6bd5697ad64fcde27d3513b09e35
ndotlara <natalialara.cd@gmail.com>
2017-10-05 00:17:22 -0400

    added modal, incomplete

--- assets/scripts/app/api.js
+++ assets/scripts/app/api.js
@@ -1,1 +1,15 @@
 const app = require('./../app.js')
+
+const addUser = function (data) {
+  return $.ajax({
+    url: app.host + '/sign-up/',
+    method: 'POST',
+    data
+  })
+}
+
+
+
+module.exports = {
+  addUser
+}
--- assets/scripts/app/ui.js
+++ assets/scripts/app/ui.js
@@ -1,1 +1,23 @@
+'use strict'
+
 const app = require('../app.js')
+// const appEvents = require('./events.js')
+
+// on sign up success -- this is the most recent
+const onSignupSuccess = function () {
+  console.log('sign-up success')
+  // $('#errorMessage').empty()
+  // $('#errorMessageModalSignUp').empty()
+  $('#registration').hide()
+  // $('.modal-footer-reg').hide()
+  // $('#loginButton2').show()
+  // $('.cancel').show()
+  // // $('#loginButton2').show()
+  $('#registration').find('input:text').val('')
+  $('#registration').find('input:password').val('')
+  $('#signUpSuccess').prepend('<div class="row" style="text-align: center; color: black"> <p>You are now signed up. Please login. </p></div>')
+}
+
+module.exports = {
+  onSignupSuccess
+}
