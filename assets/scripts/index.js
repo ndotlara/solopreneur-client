caefa83f5bbe4e0ae5d8ac7a09b2a2216461c731
ndotlara <natalialara.cd@gmail.com>
2017-10-06 06:11:52 -0400

    WIP - html not done, CSS needs work. committing bc

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,21 +2,12 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+const appEvents = require('../scripts/app/events.js')
+// const getFormFields = require('../../lib/get-form-fields')
 
 $(() => {
   setAPIOrigin(location, config)
-})
-
-// use require with a reference to bundle the file and use it in this file
-// const example = require('./example')
-
-// // use require without a reference to ensure a file is bundled
-// require('./example')
-
-const appEvents = require('../scripts/app/events.js')
-const getFormFields = require('../../lib/get-form-fields')
-
-$(() => {
+  $('#on-login-cover').hide()
   $('#registration').on('submit', appEvents.registerUser)
   $('#login').on('submit', appEvents.loginUser)
   $('#log-out-btn').on('click', appEvents.logoutUser)
