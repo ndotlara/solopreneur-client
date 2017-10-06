93bc7c3306a427d213dd6c53274a2ddaf29404e7
ndotlara <natalialara.cd@gmail.com>
2017-10-05 23:44:12 -0400

    updated config for API origin before deployment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 const config = {
   apiOrigins: {
-    // production: 'https://solopreneur.herokuapp.com'
-    production: 'http://localhost:4741'
+    production: 'https://solopreneur.herokuapp.com',
+    development: 'http://localhost:4741'
   }
 }
 
